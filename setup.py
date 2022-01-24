259be4f109191c2c87987f9bd7444e9988d1af98
Darkhan Baimyrza <darkhan@baimyrza.com>
2022-01-23 22:44:22 -0800

    Add release and dev dependencies + dev tools

--- setup.py
+++ setup.py
@@ -1,35 +1,37 @@
-from setuptools import setup, find_packages
-import pathlib
+from pathlib import Path
 
-here = pathlib.Path(__file__).parent.resolve()
+from setuptools import find_packages, setup
 
-long_description = (here / 'README.md').read_text(encoding='utf-8')
-
+long_description = (Path(__file__).parent.resolve() / "README.md").read_text(
+    encoding="utf-8"
+)
 setup(
-    name='crawler',
-    version='0.0.0',
-    description='A Web Crawler',
+    name="crawler",
+    version="0.0.0",
+    description="A Web Crawler",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/darkslab/Crawler',
-    author='Darkhan Baimyrza',
-    author_email='darkhan@baimyrza.com',
+    long_description_content_type="text/markdown",
+    url="https://github.com/darkslab/Crawler",
+    author="Darkhan Baimyrza",
+    author_email="darkhan@baimyrza.com",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
     ],
-    keywords='crawler',
-    package_dir={'': 'src'},
-    packages=find_packages(where='src'),
-    python_requires='>=3.8, <4',
-    install_requires=[],
+    keywords="crawler",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.8, <4",
+    install_requires=[
+        "click>=7,<8",
+    ],
     entry_points={
-        'console_scripts': [
-            'crawler=crawler:main',
+        "console_scripts": [
+            "crawler=crawler:main",
         ],
     },
 )
